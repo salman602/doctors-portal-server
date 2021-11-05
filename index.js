4d45928c0a70a3e423b06e0cf5ed778b008dae5c
salman602 <salmanrahman602@gmail.com>
2021-11-05 10:11:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// Middle Ware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2tgv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run (){
+    try{
+        await client.connect();
+        console.log('Database is connected')
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello Doctors portal!')
+})
+
+app.listen(port, () => {
+  console.log(`Doctor's portal is listening port ${port}`)
+})
