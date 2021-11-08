7d4760f499a251a0c69f3f6cbf6164b8dc809c12
salman602 <salmanrahman602@gmail.com>
2021-11-08 11:10:13 +0600

    fixed admin role

--- index.js
+++ index.js
@@ -14,52 +14,74 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run (){
-    try{
-        await client.connect();
-        const database = client.db('doctors_portal');
-        const appointmentsCollection = database.collection('appointments');
-        const usersCollection = database.collection('users');
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('doctors_portal');
+    const appointmentsCollection = database.collection('appointments');
+    const usersCollection = database.collection('users');
 
-        app.get('/appointments', async (req, res) =>{
-          const email = req.query.email;
-          const date =req.query.date;
-          
-          const query = {email: email, date: date};
-          const cursor = appointmentsCollection.find(query);
-          const appointment = await cursor.toArray();
-          res.json(appointment)
-        });
+    app.get('/appointments', async (req, res) => {
+      const email = req.query.email;
+      const date = req.query.date;
 
-        app.post('/appointments', async (req, res) =>{
-          const appointment = req.body;
-          const result = await appointmentsCollection.insertOne(appointment);
-          
-          res.json(result)
-        });
+      const query = { email: email, date: date };
+      const cursor = appointmentsCollection.find(query);
+      const appointment = await cursor.toArray();
+      res.json(appointment)
+    });
 
-        // store email, password login data
-        app.post('/users', async (req, res) => {
-          const user = req.body;
-          const result = await usersCollection.insertOne(user);
-          console.log(result);
-          res.json(result);
-        });
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment);
 
-        // store google login data
-        app.put('/users', async (req, res)=>{
-          const user = req.body;
-          const filter = { email: user.email };
-          const options = { upsert: true };
-          const updateDoc = {$set: user};
-          const result = await usersCollection.updateOne(filter, updateDoc, options);
-          res.json(result);
-        });
+      res.json(result)
+    });
 
-    }
-    finally{
-      // await client.close();
-    }
+    // store email, password login data
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    // make admin who can only add an admin not all users
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+
+    })
+
+    // store google login data
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    // Update an user to an admin
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: 'admin' } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    })
+
+  }
+  finally {
+    // await client.close();
+  }
 }
 run().catch(console.dir)
 
